01cc76db656fc9480d9d27d0ace9a00d42c3ff07
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-08-08 15:16:56 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,26 @@
+require 'pry' 
+
+class Backer 
+  attr_accessor :name
+  attr_reader :backed_projects, :project
+  
+  def initialize(name) 
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+
+
+
+
+
+
+
+
+
+  
+end
---
+++ lib/project.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Project 
+  
+  attr_accessor :backers
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title
+    @backers =[]
+  end 
+  
+  def add_backer(backer)
+    @backers << backer 
+    backer.backed_projects << project
+  end
+ 
+ 
+ 
+end
